fc79f00e36c43b2a1eac9b6c7958cf7db875dfa0
Damiano Bertuna <damiano.bertuna@docebo.com>
2023-12-08 14:43:50 +0100

    Installed rxjs and used Subject instead of event emitter

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipes.model';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -9,16 +8,6 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
-
-  constructor(private recipeService: RecipeService) {
-  }
-
-  ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    );
-  }
+  constructor() {}
+  ngOnInit() {}
 }
