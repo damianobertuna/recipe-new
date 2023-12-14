3fd449fef6ade21e48bf55d7be2467cf7df19d38
Damiano Bertuna <damiano.bertuna@docebo.com>
2023-12-14 22:32:55 +0100

    added reverse and sort pipe

---
+++ src/app/pipe/order.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'order'
+})
+export class OrderPipe implements PipeTransform {
+
+  transform(value, ...args: unknown[]): unknown {
+    return value.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+  }
+
+}
