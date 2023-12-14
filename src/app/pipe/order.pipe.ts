5c8400f31ad7c17eacbdeb8ec182510850126e0f
Damiano Bertuna <damianobertuna@gmail.com>
2023-12-14 22:39:57 +0100

    test

--- src/app/pipe/order.pipe.ts
+++ src/app/pipe/order.pipe.ts
@@ -6,7 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class OrderPipe implements PipeTransform {
 
   transform(value, ...args: unknown[]): unknown {
-    return value.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+    return value.sort(
+      (a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)
+    );
   }
 
 }
