b29d26311b3a24c99a4f0f7dd744af07067de067
Damiano Bertuna <damianobertuna@gmail.com>
2023-12-18 22:30:13 +0100

    minor fix and removed links form header

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -3,14 +3,24 @@ import { Recipe } from './recipes.model';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Observable } from 'rxjs';
+import { RecipeService } from './recipe.service';
 
 @Injectable()
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();
+
+    if (recipes.length > 0) {
+      return recipes;
+    } else {
+      return this.dataStorageService.fetchRecipes();
+    }
   }
 
 }
