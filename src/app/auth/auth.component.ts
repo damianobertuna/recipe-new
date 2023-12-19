076e6da09f8b86740629506cb45babb20b72c98f
Damiano Bertuna <damianobertuna@gmail.com>
2023-12-19 09:44:38 +0100

    implemented reactive authentication form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  isLoginMode = true;
+  loginForm: FormGroup;
 
+  ngOnInit() {
+    this.loginForm = new FormGroup<any>({
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'password': new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+    this.loginForm.reset();
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 }
