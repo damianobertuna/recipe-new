bb5fe3ac5faa0a6c9d1a6d5c1642308f62ab3a98
Damiano Bertuna <damianobertuna@gmail.com>
2023-12-21 16:19:43 +0100

    Hidden menu voices when user authenticate

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
-  constructor(private dataStorage: DataStorageService) {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  subscription = new Subscription();
+
+  constructor(
+    private dataStorage: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      console.log(!user);
+      console.log(!!user);
+    });
   }
 
-  ngOnInit() {}
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   onSaveData() {
     this.dataStorage.storeRecipes();
