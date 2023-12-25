6f08775b21b9794d7c4844ea2ff8dbd9e5f18a10
Damiano Bertuna <damianobertuna@gmail.com>
2023-12-25 10:54:28 +0100

    Added core module with common services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing';
 import { FormTdComponent } from './form-td/form-td.component';
 import { FormReactiveComponent } from './form-reactive/form-reactive.component';
-import { RecipeService } from './recipes/recipe.service';
 import { PipeComponent } from './pipe/pipe.component';
 import { ReversePipe } from './pipe/reverse.pipe';
 import { OrderPipe } from './pipe/order.pipe';
-import { DataStorageService } from './shared/data-storage.service';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptor } from './auth/auth.interceptor';
-import { AuthGuard } from './auth/auth.guard';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from '../core.module';
 
 @NgModule({
   declarations: [
@@ -41,15 +36,8 @@ import { SharedModule } from './shared/shared.module';
     ReactiveFormsModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    DataStorageService,
-    RecipesResolverService,
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
-    AuthGuard
+    SharedModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
